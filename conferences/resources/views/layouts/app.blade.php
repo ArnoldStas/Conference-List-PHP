e20e2b563dc73bcca305592c85ddde7eef40a345
ArnoldStas <arnoldasstasiunas@gmail.com>
2025-11-23 18:11:10 +0200

    Removed all rights reserved footer from the main layout file (Not required)

--- conferences/resources/views/layouts/app.blade.php
+++ conferences/resources/views/layouts/app.blade.php
@@ -77,16 +77,6 @@
         </div>
     </main>
 
-
-    <footer class="bg-light text-center text-muted py-3 mt-5">
-        <div class="container">
-            <p class="mb-0">
-                &copy; {{ date('Y') }} {{ config('app.name', 'Conference Management') }}.
-                {{ __('messages.all_rights_reserved') }}
-            </p>
-        </div>
-    </footer>
-
     @vite(['resources/js/app.js'])
 </body>
 </html>
